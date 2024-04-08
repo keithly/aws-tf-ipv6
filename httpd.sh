6a6c7e1c724c76402dce8dc1528e6641df157751
Keith Petersen <krp@keithrpetersen.com>
2024-04-08 16:37:43 -0500

    use dnf instead of yum

--- httpd.sh
+++ httpd.sh
@@ -3,9 +3,9 @@
 # for debugging boot failures
 exec > /tmp/userdata.log 2>&1
 
-yum -y update
+dnf -y update
 
-sudo yum install httpd -y
+sudo dnf install httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
 echo "Hello World" | sudo tee /var/www/html/index.html
